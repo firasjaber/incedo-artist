065bd2bde9a47303660908e845551c9eaae333c7
Firas Jaber <firasjaberc@gmail.com>
2021-12-05 13:11:03 +0100

    helper jsdoc

--- src/helpers/randomArtists.ts
+++ src/helpers/randomArtists.ts
@@ -1,6 +1,10 @@
 import { promises as fs } from 'fs';
 import path from 'path';
 
+/**
+ * Retrieve random artists from JSON system file
+ * @returns Array<{ name : "example" }>
+ */
 export const randomArtists = async () => {
   const buffer = await fs.readFile(path.join(__dirname, 'artists.json'));
   const { artists } = JSON.parse(buffer.toString());
