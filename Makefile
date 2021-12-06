b71f028dd9feb96a83e66a7e0f043ce48e956e14
usersina <benna.anis2@gmail.com>
2021-12-06 12:20:55 +0100

    Merge dockerizing-2

--- Makefile
+++ Makefile
@@ -5,7 +5,4 @@ build:
 	docker build -t artists-api -f Dockerfile .
 
 run:
-	docker run -p 9000:9000 --name=artists artists-api
-
-build-and-run:
-	$(MAKE) build && $(MAKE) run
+	docker run -p 9000:9000 -d artists-api
--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,3 @@
 export const LASTFM_API_KEY = process.env.LASTFM_API_KEY;
 export const BASE_URL = `https://ws.audioscrobbler.com/2.0/?api_key=${LASTFM_API_KEY}&format=json&method=artist.search`;
+export const PORT = process.env.PORT;
